d21a94063137607df5373fc03980940fe107d533
ajcasas-su <123099924+ajcasas-su@users.noreply.github.com>
2023-05-04 22:32:43 -0400

    project 7

--- project/project07/script.js
+++ project/project07/script.js
@@ -35,11 +35,26 @@ const observerbioRight = new IntersectionObserver((entries) => {
 
 });
 
+const observerbackground = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('showbackground')
+        } else {
+            entry.target.classList.remove('showbackground')
+        }
+    });
+
+});
+
+
 const hiddenElements = document.querySelectorAll('.hidden');
 const hiddenElementsbioLeft = document.querySelectorAll('.hiddenbioLeft');
 const hiddenElementsbioRight = document.querySelectorAll('.hiddenbioRight');
+const hiddenElementsbackground = document.querySelectorAll('.hiddenbackground');
 
 
 hiddenElements.forEach((el) => observer.observe(el));
 hiddenElementsbioLeft.forEach((el) => observerbioLeft.observe(el));
 hiddenElementsbioRight.forEach((el) => observerbioRight.observe(el));
+hiddenElementsbackground.forEach((el) => observerbackground.observe(el));
